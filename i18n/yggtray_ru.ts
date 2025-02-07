24b847a5621cc4a3758c2d1629cdc32c31fb99c6
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2025-02-07 20:22:55 +0300

    SetupWizard.h: Translate strings
    * SetupWizard.h: Translate strings.
    * i18n/yggtray_ru.ts: Add translations for the SetupWizard.

--- i18n/yggtray_ru.ts
+++ i18n/yggtray_ru.ts
@@ -60,4 +60,121 @@
       </translation>
     </message>
   </context>
+  <context>
+    <name>QObject</name>
+    <message>
+      <source>Group Membership</source>
+      <translation>Членство в группах</translation>
+    </message>
+    <message>
+      <source>You are not in the 'yggdrasil' group. To use this application, you must be added to this group.</source>
+      <translation>Ваш пользователь не добавлен в группу 'yggdrasil'. Для того, чтобы использовать это приложение, вам необходимо добавить его в эту группу.</translation>
+    </message>
+    <message>
+      <source>Would you like to add yourself to the 'yggdrasil' group now?</source>
+      <translation>Вы хотите добавить себя в группу 'yggdrasil'?</translation>
+    </message>
+    <message>
+      <source>Add Me</source>
+      <translation>Добавить меня</translation>
+    </message>
+    <message>
+      <source>Skip</source>
+      <translation>Пропустить</translation>
+    </message>
+    <message>
+      <source>Setup Incomplete</source>
+      <translation>Установка не завершена</translation>
+    </message>
+    <message>
+      <source>You need to be in the 'yggdrasil' group to use the application. Exiting setup.</source>
+      <translation>Вы должны быть в группе 'yggdrasil' для того, чтобы использовать это приложение. Настройка прервана.</translation>
+    </message>
+    <message>
+      <source>Would you like to configure ip6tables for Yggdrasil?</source>
+      <translation>Хотите настроить ip6tables для Yggdrasil?</translation>
+    </message>
+    <message>
+      <source>Configure</source>
+      <translation>Настроить</translation>
+    </message>
+    <message>
+      <source>Setup Wizard</source>
+      <translation>Мастер настройки</translation>
+    </message>
+    <message>
+      <source>Failed to mark the setup as complete.</source>
+      <translation>Не получилось пометить установку, как завершённую</translation>
+    </message>
+    <message>
+      <source>Group Addition</source>
+      <translation>Добавление группы</translation>
+    </message>
+    <message>
+      <source>You have been added to the '%1' group. Please log out and log back in for the changes to take effect.</source>
+      <translation>Вы были добавлены в группу '%1'. Пожалуйста, выйдите из системы и войдите обратно, чтобы изменения применились.</translation>
+    </message>
+    <message>
+      <source>Failed to add you to the '%1' group. Ensure you have the necessary permissions.</source>
+      <translation>Не получилось добавить вас в группу '%1'. Удостоверьтесь, что у вас достаточно прав для совершения действия.</translation>
+    </message>
+    <message>
+      <source>The ip6tables configuration file already exists. What would you like to do?</source>
+      <translation>Файл конфигурации ip6tables уже существует. Что вы хотите сделать?</translation>
+    </message>
+    <message>
+      <source>Overwrite</source>
+      <translation>Перезаписать</translation>
+    </message>
+    <message>
+      <source>Append</source>
+      <translation>Добавить</translation>
+    </message>
+    <message>
+      <source>Cancel</source>
+      <translation>Отменить</translation>
+    </message>
+    <message>
+      <source>No changes were made to the ip6tables configuration.</source>
+      <translation>Конфигурация ip6tables не была изменена.</translation>
+    </message>
+    <message>
+      <source>Failed to create a temporary file for ip6tables rules.</source>
+      <translation>Не удалось создать временный файл для сохранения ip6tables правил.</translation>
+    </message>
+    <message>
+      <source>Rules have been appended to the configuration.</source>
+      <translation>Правила были добавлены в конфиграцию.</translation>
+    </message>
+    <message>
+      <source>Rules have been written to the configuration.</source>
+      <translation>Правила были записаны в конфиграцию.</translation>
+    </message>
+    <message>
+      <source>Failed to write to the ip6tables configuration file. Ensure you have the necessary permissions.</source>
+      <translation>Не удалось записать правила в файл конфиграции ip6tables. Убедитесь, что у вас есть достаточно прав для данного действия.</translation>
+    </message>
+    <message>
+      <source>ip6tables Service</source>
+      <translation>Сервис ip6tables</translation>
+    </message>
+    <message>
+      <source>The ip6tables service has been enabled and started successfully.</source>
+      <translation>Сервис ip6tables был включен и запущен успешно.</translation>
+    </message>
+    <message>
+      <source>Failed to enable and start the ip6tables service. Ensure it is properly installed.</source>
+      <translation>Не удалось включить и запустить сервис ip6tables.  Убедитесь, что он корректно установлен.</translation>
+    </message>
+    <message>
+      <source></source>
+      <translation></translation>
+    </message>
+    <!--
+    <message>
+      <source></source>
+      <translation></translation>
+    </message>
+    -->
+  </context>
 </TS>
